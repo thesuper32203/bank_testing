5f5984a377bf52fd0be2ae577a04067f0067a6ea
Anthony <thesuper32203@gmail.com>
2025-10-22 20:03:50 -0400

    created function to get api key safely

--- src/main/java/BankTesting/app.java
+++ src/main/java/BankTesting/app.java
@@ -1,5 +1,21 @@
 package BankTesting;
 
-public class app {
+import io.github.cdimascio.dotenv.Dotenv;
 
+public class app {
+    public static void main(String[] args) {
+        // Load env vars from .env
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        String appKey = getenv("FINICITY_APP_KEY", dotenv);
+        System.out.println("App started. FINICITY_APP_KEY present? " + (appKey != null));
+    }
+
+    private static String getenv(String key, Dotenv dotenv){
+
+        String v = System.getenv(key);
+        if (v == null && dotenv != null) v = dotenv.get(key);
+        return v;
+
+    }
 }
